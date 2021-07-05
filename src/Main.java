0facc5596165b9d0c4dc66825391a39e30f09a36
Theodora2000 <xsimont@stuba.sk>
2021-07-05 12:05:16 +0200

    provera da li tekst sadrzi datu rec

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String args[]){
         //tekst je rreferenca na neke stringovne objekte
         String tekst = "Neki kratki tekst.";
-        String tekst2 = "neki drugi tekst";
+        String rec = "kratki";
 
 
-        if(tekst.equals(tekst2)){
-            System.out.println("Jednaki su");
+        if(tekst.contains(rec)){
+            System.out.println("Sadrzi");
         }else{
-            System.out.println("Nisu");
+            System.out.println("Ne sadrzi");
         }
 
         System.out.println(tekst);
