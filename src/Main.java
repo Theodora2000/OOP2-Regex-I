9a992dd5d7e5f0d35c9aac7cf6d3097b235d5cd0
Theodora2000 <xsimont@stuba.sk>
2021-07-05 12:51:06 +0200

    regex - regularani izrazi - \\w sve sto moze da se nalazi u reci

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,8 @@ import java.util.regex.Pattern;
 public class Main {
 
 
-            private static final Pattern REGEX = Pattern.compile("[aeiou]");
-            //uhvati samo jedna vilo koji znak od sva ponudjena
-    //grupa znakova
+            private static final Pattern REGEX = Pattern.compile("\\w");
+
     private static final String[] INPUT = {
                     "Neki kratak tekst u kojem   se rec tekst javlja vise puta. Takodje, tekst sadrzi i specijalne znake poput poput tacke i zagrada: ( . ) % $ [ , ] \\ /",
                     "Ana voli Milovana!",
