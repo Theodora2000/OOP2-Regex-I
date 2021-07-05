326178ccb6878cc2b7bfe5e3d05894a783e27eb2
Theodora2000 <xsimont@stuba.sk>
2021-07-05 12:11:02 +0200

    regex

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String args[]){
-        //tekst je rreferenca na neke stringovne objekte
-        String tekst = "Neki kratki tekst.";
-        String rec = "kratki";
+        //regularni izraz -  to su stringovi pomocu koijh mozemo da opisemo jezik,
+        //odnosno skup stringova koji pripadaju tom nekom jeziku
+
+        String tekst = "Neki kratka tekst.";
+        String rec = "kratak";
 
 
-        if(tekst.contains(rec)){
+        if(tekst.contains(rec) || tekst.contains("kratka") || tekst.contains("kratko")){
             System.out.println("Sadrzi");
         }else{
             System.out.println("Ne sadrzi");
