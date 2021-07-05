b54993c498d30beaa233fb27ab26744dac961f47
Theodora2000 <xsimont@stuba.sk>
2021-07-05 12:01:43 +0200

    ispavan nacin - porediti stringove

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,10 @@ public class Main {
     public static void main(String args[]){
         //tekst je rreferenca na neke stringovne objekte
         String tekst = "Neki kratki tekst.";
-
-
-        //stringovi se ne porede sa == nego sa equals
-
-        //ovo nije dobar nacin porejdenja jer su stringovi objekti, referencijalni tip podataka
         String tekst2 = "neki drugi tekst";
-        if(tekst==tekst2){
+
+
+        if(tekst.equals(tekst2)){
             System.out.println("Jednaki su");
         }else{
             System.out.println("Nisu");
