7087156a4251b265fcebb8d54714bb33cfb8b083
Theodora2000 <xsimont@stuba.sk>
2021-07-05 12:14:21 +0200

    regex - deljenje - beleska

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ public class Main {
         //odnosno skup stringova koji pripadaju tom nekom jeziku
 
         String tekst = "Neki kratka tekst.";
-
+        //stringovi mogu da se koriste kako regularni izrazi,
+        //medjutim sami regularni izrazi kao string ne
         String[] reci = tekst.split(" ");
         for (String rec : reci){
             System.out.println(rec);
