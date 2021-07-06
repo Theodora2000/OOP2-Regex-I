1294c90389cd1df2500789ba63b53f602a48b92d
Theodora2000 <xsimont@stuba.sk>
2021-07-06 12:24:51 +0200

    regex 2.cas - inostrani + nasi

--- src/RegexExample2.java
+++ src/RegexExample2.java
@@ -4,8 +4,9 @@ import java.util.regex.Pattern;
 public class RegexExample2 {
     public static void main(String args[]){
         String tekst = "060 123456789 | NS 123-AB | 061/234-56-78 | +381 21 123 456 | 194 | noreply@somedomain.com | studentska@pmf.uns.ac.rs | ime.prezime@domen.rs | 123-456";
-        String regex = " (\\+\\d{1,3} \\d{2}|0\\d{2})[ /]\\d{2,}([ -]\\d{2,})* ";
+        String regex = " ((\\+\\d{1,3} \\d{2}|0\\d{2})[ /])?\\d{2,}([ -]\\d{2,})* ";
         //prvo neki niz cifara pa onda 0 ili vise koji su odvojeni sa necime
+        //((\+\d{1,3} \d{2}|0\d{2})[ /])? ovaj deo moze a ne mora da se javi
         Pattern pattern  = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(tekst);
         while (matcher.find()) {
