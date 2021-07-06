426553aecccbc69dca11e7a6d6be31ca28ec2f9f
Theodora2000 <xsimont@stuba.sk>
2021-07-06 12:35:26 +0200

    regex 2.cas - telefoni kompletno

--- src/RegexExample2.java
+++ src/RegexExample2.java
@@ -4,19 +4,17 @@ import java.util.regex.Pattern;
 public class RegexExample2 {
     public static void main(String args[]){
         String tekst = "060 123456789 | NS 123-AB | 061/234-56-78 | +381 21 123 456 | 194 | noreply@somedomain.com | studentska@pmf.uns.ac.rs | ime.prezime@domen.rs | 123-456";
-        String regex = " ((\\+\\d{1,3} \\d{2}|0\\d{2})[ /])?\\d{2,}([ -]\\d{2,})*";
+        String regex = " (?:(?<pozivni>\\+\\d{1,3} \\d{2}|0\\d{2})[ /])?(?<osnovni>\\d{2,}(?:[ -]\\d{2,})*)";
         //prvo neki niz cifara pa onda 0 ili vise koji su odvojeni sa necime
         //((\+\d{1,3} \d{2}|0\d{2})[ /])? ovaj deo moze a ne mora da se javi
         Pattern pattern  = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(tekst);
         while (matcher.find()) {
             System.out.println(matcher.group(0));
-			System.out.println("      0: " + matcher.start(0) + "-" + matcher.end(0) + ": " + matcher.group(0));
-			System.out.println("      1: " + matcher.start(1) + "-" + matcher.end(1) + ": " + matcher.group(1));
-			System.out.println("      2: " + matcher.start(2) + "-" + matcher.end(2) + ": " + matcher.group(2));
-            /*System.out.println("pozivni: " + matcher.start("pozivni") + "-" + matcher.end("pozivni") + ": " + matcher.group("pozivni"));
+
+			System.out.println("pozivni: " + matcher.start("pozivni") + "-" + matcher.end("pozivni") + ": " + matcher.group("pozivni"));
             System.out.println("osnovni: " + matcher.start("osnovni") + "-" + matcher.end("osnovni") + ": " + matcher.group("osnovni"));
-            System.out.println();*/
+            System.out.println();
         }
     }
 }
